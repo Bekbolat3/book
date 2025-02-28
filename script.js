39df70cc5b3df9db06ec20c6902f27b1cce081b2
Bekbolat3 <orynbasarbekbolat06@gmail.com>
2025-02-28 13:09:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+function loadContent(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        });
+}
